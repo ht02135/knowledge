3b479129d9a29091f43b7a42efff9a2816166465
Hung En Tsai <ht02135@yahoo.com>
2016-08-17 10:58:14 -0400

    xmlDoc and xPath example

---
+++ java/xpath/xmlparser.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+	public ArrayList<String> getXMLValue(String xPathExpression, String xmlFile){
+		ArrayList<String> list = new ArrayList<String>();
+		try {
+			FileInputStream file = new FileInputStream(new File(xmlFile));
+			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+			Document xmlDocument = builder.parse(file);
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+//			String expression = "//Form[@id='userProfileContactInformation']/field[@required='true']/@id";
+			
+			NodeList nodeList = (NodeList) xPath.compile(xPathExpression).evaluate(xmlDocument, XPathConstants.NODESET);
+			System.out.println("nodeList.getLength() "+nodeList.getLength());
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				System.out.println(nodeList.item(i).getTextContent());
+				String result = nodeList.item(i).getFirstChild().getNodeValue();
+				list.add(result);
+			}
+			System.out.println("list="+list);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
