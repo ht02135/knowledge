a9b187726f220897d8bd0a0c7dabb4bd970c0d90
Hung En Tsai <ht02135@yahoo.com>
2018-06-13 16:46:21 -0400

    Update index_query.sql

--- java/index/index_query.sql
+++ java/index/index_query.sql
@@ -10,7 +10,12 @@ object_id	name	index_id	type	type_desc
 289501659	idx_batch_step_job_execution_id	5	2	NONCLUSTERED
 337501830	idx_id	1	1	CLUSTERED
 */
-SELECT * 
+SELECT 
+    DB_NAME(object_id) AS 'DB_NAME',
+    OBJECT_SCHEMA_NAME(object_id) AS OBJECT_SCHEMA_NAME,
+	OBJECT_NAME(object_id) as 'OBJECT_NAME',
+	object_id as 'OBJECT_ID',
+	*
 FROM sys.indexes 
 WHERE 1=1
 and name in (
@@ -18,10 +23,19 @@ and name in (
 	'idx_id',
 	'idx_batch_step_job_execution_id',
 	'idx_batch_job_execution_instance_id')
-and (object_id = OBJECT_ID('[aloha_user_prod].[dbo].[BATCH_EXECUTION_CONTEXT]') or
-     object_id = OBJECT_ID('[aloha_user_prod].[dbo].[BATCH_JOB_EXECUTION_SEQ]') or
-     object_id = OBJECT_ID('[aloha_user_prod].[dbo].[BATCH_JOB_SEQ]') or
-     object_id = OBJECT_ID('[aloha_user_prod].[dbo].[BATCH_STEP_EXECUTION_SEQ]') or
-     object_id = OBJECT_ID('[aloha_user_prod].[dbo].[BATCH_STEP_EXECUTION]') or
-     object_id = OBJECT_ID('[aloha_user_prod].[dbo].[BATCH_JOB_EXECUTION]')
+and (object_id = OBJECT_ID('[dbo].[BATCH_EXECUTION_CONTEXT]') or
+     object_id = OBJECT_ID('[dbo].[BATCH_JOB_EXECUTION_SEQ]') or
+     object_id = OBJECT_ID('[dbo].[BATCH_JOB_SEQ]') or
+     object_id = OBJECT_ID('[dbo].[BATCH_STEP_EXECUTION_SEQ]') or
+     object_id = OBJECT_ID('[dbo].[BATCH_STEP_EXECUTION]') or
+     object_id = OBJECT_ID('[dbo].[BATCH_JOB_EXECUTION]')
 )
+
+check if index is created for tables.  or you just go to tables and drill down index from microsoft sql studio
+
+BATCH_EXECUTION_CONTEXT
+BATCH_JOB_SEQ
+BATCH_JOB_EXECUTION_SEQ
+BATCH_JOB_EXECUTION
+BATCH_STEP_EXECUTION_SEQ
+BATCH_STEP_EXECUTION
