1eb8318d20fd7c014cb23084db233235e6b9eaba
Hung En Tsai <ht02135@yahoo.com>
2018-09-19 11:40:00 -0400

    Update join_criteria.sql

--- java/sql/join_criteria.sql
+++ java/sql/join_criteria.sql
@@ -1,60 +1,89 @@
-
 1>criteria in join
 
 SELECT '' as 'ct+ctm',
        ct.subscription_ind as 'ct.subscription_ind', 
-       ct.communication_template_type as 'ct.communication_template_type',
-       ct.communication_template_code as 'ct.communication_template_code', 
+       ct.CT_type as 'ct.CT_type',
+       ct.CT_code as 'ct.CT_code', 
        ct.status as 'ct.status',
        ctm.metadata_id as 'ctm.metadata_id',
        ctm.name as 'ctm.name',
        ctm.meta_value as 'ctm.meta_value',
        ctm.*
-FROM communication_template as ct WITH(NOLOCK) 
-left join communication_template_metadata as ctm with (nolock) on ctm.communication_template_id = ct.communication_template_id and ctm.name in ('ListEffectiveDays')
+FROM CT as ct WITH(NOLOCK) 
+left join CTM as ctm with (nolock) on ctm.CT_id = ct.CT_id and ctm.name in ('EffectiveDays')
 WHERE 1=1
 and ct.status='ACTIVE'
-and ct.communication_template_code like '%seminar%'
-and ct.communication_template_code in (
-	'women_and_wealth_seminar',
-	'seminar_invitation_postcards_dm',
-	'seminar_express_seminar_pkg'
+and ct.CT_code like '%seminar%'
+and ct.CT_code in (
+	'woman',
+	'invitation',
+	'pkg'
 )
-ORDER BY ct.subscription_ind desc, ct.communication_template_type desc, ct.communication_template_code desc
+ORDER BY ct.subscription_ind desc, ct.CT_type desc, ct.CT_code desc
 
 it returns null and let you know which is missing
-ct.subscription_ind	ct.communication_template_type	ct.communication_template_code	ct.status	ctm.metadata_id	ctm.name	ctm.meta_value
-0	Direct Mail	women_and_wealth_seminar	ACTIVE	178	ListEffectiveDays	30
-0	Direct Mail	seminar_invitation_postcards_dm	ACTIVE	NULL	NULL	NULL
-0	Direct Mail	seminar_express_seminar_pkg	ACTIVE	171	ListEffectiveDays	30
+ct.subscription_ind	ct.CT_type	ct.CT_code	ct.status	ctm.metadata_id	ctm.name	ctm.meta_value
+0	Direct Mail	woman	ACTIVE	178	EffectiveDays	30
+0	Direct Mail	invitation	ACTIVE	NULL	NULL	NULL
+0	Direct Mail	pkg	ACTIVE	171	EffectiveDays	30
 
+///////////////////////////////////////////////
+
+DECLARE @userId NUMERIC(19, 0);
+SELECT @userId = user_id from users with(nolock) where user_name = 'haha';
+
+INSERT INTO CTM
+( version
+, name
+, meta_value
+, created_by
+, creation_date
+, CT_id
+)
+SELECT 0 as version
+, 'EffectiveDays' as name
+, '90' as meta_value
+, @userId
+, GETDATE() as creation_date
+, ct.CT_id
+FROM CT as ct WITH(NOLOCK) 
+left join CTM as ctm with (nolock) on ctm.CT_id = ct.CT_id and ctm.name in ('EffectiveDays')
+where 1=1
+and ct.CT_code in (
+	'woman',
+	'invitation',
+	'pkg'
+)
+and ctm.metadata_id is null
+
+////////////////////////////////////////////////
 VS
 
 2>criteria in where
 
 SELECT '' as 'ct+ctm',
        ct.subscription_ind as 'ct.subscription_ind', 
-       ct.communication_template_type as 'ct.communication_template_type',
-       ct.communication_template_code as 'ct.communication_template_code', 
+       ct.CT_type as 'ct.CT_type',
+       ct.CT_code as 'ct.CT_code', 
        ct.status as 'ct.status',
        ctm.metadata_id as 'ctm.metadata_id',
        ctm.name as 'ctm.name',
        ctm.meta_value as 'ctm.meta_value',
        ctm.*
-FROM communication_template as ct WITH(NOLOCK) 
-left join communication_template_metadata as ctm with (nolock) on ctm.communication_template_id = ct.communication_template_id
+FROM CT as ct WITH(NOLOCK) 
+left join CTM as ctm with (nolock) on ctm.CT_id = ct.CT_id
 WHERE 1=1
 and ct.status='ACTIVE'
-and ct.communication_template_code like '%seminar%'
-and ct.communication_template_code in (
-	'women_and_wealth_seminar',
-	'seminar_invitation_postcards_dm',
-	'seminar_express_seminar_pkg'
+and ct.CT_code like '%seminar%'
+and ct.CT_code in (
+	'woman',
+	'invitation',
+	'pkg'
 )
-and ctm.name in ('ListEffectiveDays')
-ORDER BY ct.subscription_ind desc, ct.communication_template_type desc, ct.communication_template_code desc
+and ctm.name in ('EffectiveDays')
+ORDER BY ct.subscription_ind desc, ct.CT_type desc, ct.CT_code desc
 
 it outright exclude the null
-ct.subscription_ind	ct.communication_template_type	ct.communication_template_code	ct.status	ctm.metadata_id	ctm.name	ctm.meta_value
-0	Direct Mail	women_and_wealth_seminar	ACTIVE	178	ListEffectiveDays	30
-0	Direct Mail	seminar_express_seminar_pkg	ACTIVE	171	ListEffectiveDays	30
+ct.subscription_ind	ct.CT_type	ct.CT_code	ct.status	ctm.metadata_id	ctm.name	ctm.meta_value
+0	Direct Mail	woman	ACTIVE	178	EffectiveDays	30
+0	Direct Mail	pkg	ACTIVE	171	EffectiveDays	30
