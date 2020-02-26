3a1636a752b86625e80799f3d7792b2ffe435a6e
Hung En Tsai <ht02135@yahoo.com>
2020-02-26 16:56:49 -0500

    Update count_distinct_case_when.sql

--- java/sql/count_distinct_case_when.sql
+++ java/sql/count_distinct_case_when.sql
@@ -15,3 +15,17 @@ Zeros or negative values would be evaluated as null and won't be included in cou
 
 	,count(distinct case when qpcode in (1020,1021) then email_address end) as attribute6--unique open count
 	,count(distinct case when qpcode in (1071,1070) then email_address end) as attribute8--unique click count
+		  
+///////////
+		  
+select ct.*
+from communication_template as ct with(nolock)
+where 1=1
+and ct.communication_template_type in ('Email')
+
+select
+	count(case when ct.communication_template_type in ('Email') then ct.reporting_segment end) as count,
+	count(distinct case when ct.communication_template_type in ('Email') then ct.reporting_segment end) as unique_count
+from communication_template as ct with(nolock)
+		  
+		  
