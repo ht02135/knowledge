77046112812c7a4eaea6000d0a4e617def79a8b9
Hung En Tsai <ht02135@yahoo.com>
2020-03-02 17:06:31 -0500

    Update bandwidth.sql

--- java/sql/bandwidth.sql
+++ java/sql/bandwidth.sql
@@ -14,3 +14,29 @@ Group by --communication_id,
 	datepart(hour,cpe.creation_date), cpe.processing_stage, cpe.processing_status
 order by --communication_id,
 	Hour
+
+/////////////////
+				      
+				      select --communication_id,
+    datepart(year,cpe.creation_date) as Year,
+    datepart(month,cpe.creation_date) as Month,
+    datepart(day,cpe.creation_date) as Day,
+    datepart(hour,cpe.creation_date) as Hour,
+	CONVERT(VARCHAR(MAX),datepart(hour,cpe.creation_date)) + ':00:00.000' as 'military time', 
+	cpe.processing_stage,
+	cpe.processing_status,
+	COUNT(*) as 'cpe count'
+from communication_process_email cpe with (nolock)
+join communication c with (nolock) on c.communication_id = cpe.communication_id
+where  1=1
+and cpe.creation_date between @StartDate and @EndDate
+and processing_stage = 1000
+Group by --communication_id,
+    datepart(year,cpe.creation_date),
+    datepart(month,cpe.creation_date),
+    datepart(day,cpe.creation_date),
+	datepart(hour,cpe.creation_date), cpe.processing_stage, cpe.processing_status
+order by --communication_id,
+	Year,Month,Day,Hour
+				      
+				      
